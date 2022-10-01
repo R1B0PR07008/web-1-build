25cbc7aecee4687e13d2f654257f4561b48a5782
R1B0PR07008 <104583138+R1B0PR07008@users.noreply.github.com>
2022-10-01 12:58:26 -0600

    Mass program works!

--- site.js
+++ site.js
@@ -71,37 +71,44 @@ console.log("hi, welcome to WEB-1. I see you're looking at the code. I wouldn't 
 // }
 
 //* OTHER PAGES
-
-if (document.URL.includes("len.html")) {
-    console.log("you are in the len page");
     function Hide(UNIT) {
         let un = 'undefined';
         if (UNIT === un ) {
             document.getElementsByClassName("ans-text").visibility = "hidden";
             document.getElementById('ans-text').style.visibility = "hidden";
+            document.getElementById('1-LEN-IF').style.visibility = "visible";
+            document.getElementById('2-LEN-IF').style.visibility = "visible";
         } if (
-            UNIT === 'm' || UNIT === 'M' ||
-            UNIT === 'km' || UNIT === 'KM' || UNIT === 'Km' || UNIT === 'kM' ||
-            UNIT === 'cm' || UNIT === 'CM' || UNIT === 'Cm' || UNIT === 'cM' ||
-            UNIT === 'ft' || UNIT === 'FT' || UNIT === 'Ft' || UNIT === 'fT' ||
-            UNIT === 'yd' || UNIT === 'YD' || UNIT === 'Yd' || UNIT === 'yD' ||
-            UNIT === 'mi' || UNIT === 'MI' || UNIT === 'Mi' || UNIT === 'mI' ||
-            UNIT === 'in' || UNIT === 'IN' || UNIT === 'iN' || UNIT === 'In'
+            UNIT === 'M' ||
+             UNIT === 'KM' ||
+             UNIT === 'CM' || 
+             UNIT === 'FT' ||
+             UNIT === 'YD' ||
+             UNIT === 'MI' ||
+             UNIT === 'IN' ||
+
+            UNIT === 'G'
         ) {
-            document.getElementById('BTN-1-LEN-IF').style.visibility = "hidden";
+            document.getElementById('1-LEN-IF').style.visibility = "hidden";
+            document.getElementById('2-LEN-IF').style.visibility = "hidden";
             document.getElementById('ans-text').style.visibility = "visible";
+            document.getElementsByClassName("ans-text").visibility = "visible";
         };
     };
+
+if (document.URL.includes("len.html")) {
+    console.log("you are in the len page");
+
     let UNIT = document.getElementById('UNIT-LEN').value || 'undefined';
-    Hide(UNIT);
+    // Hide(UNIT);
 
     function Len() {
         let NUM = document.getElementById('NUM-LEN').value || 1;
         let UNIT = document.getElementById('UNIT-LEN').value || 'undefined';
         console.log("btn 'BTN-1-LEN' pressed.");
-        console.log(NUM)
-        console.log(UNIT)
-        if (UNIT === 'm' || UNIT === 'M') {
+        console.log(NUM);
+        console.log(UNIT);
+        if (UNIT === 'M') {
 
             document.getElementById("len-1-text").innerHTML = "M to KM";
             document.getElementById("len-2-text").innerHTML = "M to CM";
@@ -134,7 +141,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "* 39.37";
 
             Hide(UNIT)
-        } if (UNIT === 'km' || UNIT === 'KM' || UNIT === 'Km' || UNIT === 'kM') {
+        } if (UNIT === 'KM') {
             document.getElementById("len-1-text").innerHTML = "KM to M:";
             document.getElementById("len-2-text").innerHTML = "KM to CM:";
             document.getElementById("len-3-text").innerHTML = "KM to FT:";
@@ -166,7 +173,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "* 39370 ";
 
             Hide(UNIT)
-        } if (UNIT === 'cm' || UNIT === 'CM' || UNIT === 'Cm' || UNIT === 'cM') {
+        } if (UNIT === 'CM') {
             document.getElementById("len-1-text").innerHTML = "CM to M:";
             document.getElementById("len-2-text").innerHTML = "CM to KM:";
             document.getElementById("len-3-text").innerHTML = "CM to FT:";
@@ -198,7 +205,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "/ 2.54  ";
 
             Hide(UNIT)
-        } if (UNIT === 'ft' || UNIT === 'FT' || UNIT === 'Ft' || UNIT === 'fT') {
+        } if (UNIT === 'FT') {
             document.getElementById("len-1-text").innerHTML = "FT to M:";
             document.getElementById("len-2-text").innerHTML = "FT to KM:";
             document.getElementById("len-3-text").innerHTML = "FT to CM:";
@@ -230,7 +237,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "* 12    ";
 
             Hide(UNIT)
-        } if (UNIT === 'yd' || UNIT === 'YD' || UNIT === 'Yd' || UNIT === 'yD') {
+        } if (UNIT === 'YD') {
             document.getElementById("len-1-text").innerHTML = "YD to M:";
             document.getElementById("len-2-text").innerHTML = "YD to KM:";
             document.getElementById("len-3-text").innerHTML = "YD to CM:";
@@ -262,7 +269,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "/ 12    ";
 
             Hide(UNIT)
-        } if (UNIT === 'mi' || UNIT === 'MI' || UNIT === 'Mi' || UNIT === 'mI') {
+        } if (UNIT === 'MI') {
             document.getElementById("len-1-text").innerHTML = "MI to M:";
             document.getElementById("len-2-text").innerHTML = "MI to KM:";
             document.getElementById("len-3-text").innerHTML = "MI to CM:";
@@ -294,7 +301,7 @@ if (document.URL.includes("len.html")) {
             document.getElementById("len-6-formula").innerHTML = "* 63360 ";
 
             Hide(UNIT)
-        } if (UNIT === 'in' || UNIT === 'IN' || UNIT === 'iN' || UNIT === 'In') {
+        } if (UNIT === 'IN') {
             document.getElementById("len-1-text").innerHTML = "IN to M:";
             document.getElementById("len-2-text").innerHTML = "IN to KM:";
             document.getElementById("len-3-text").innerHTML = "IN to CM:";
@@ -330,3 +337,131 @@ if (document.URL.includes("len.html")) {
     };
 };
 
+if (document.URL.includes("mass.html")) {
+    console.log("you are in the mass page");
+
+    function Mass() {
+        let NUM = document.getElementById('NUM-MASS').value || 1;
+        let UNIT = document.getElementById('UNIT-MASS').value || 'undefined';
+        console.log("btn 'BTN-1-' pressed.");
+        console.log(NUM);
+        console.log(UNIT);
+
+        if (UNIT === 'KG') {
+            document.getElementById("mass-1-text").innerHTML = "KG to KG:";
+            document.getElementById("mass-2-text").innerHTML = "KG to TON:";
+            document.getElementById("mass-3-text").innerHTML = "KG to LBS:";
+            document.getElementById("mass-4-text").innerHTML = "KG to MG:";
+
+            document.getElementById("mass-5-text").innerHTML = "KG to KG:";
+            document.getElementById("mass-6-text").innerHTML = "KG to TON:";
+            document.getElementById("mass-7-text").innerHTML = "KG to LBS:";
+            document.getElementById("mass-8-text").innerHTML = "KG to MG:";
+
+            document.getElementById("01-mass").innerHTML = (NUM * 1000 );
+            document.getElementById("02-mass").innerHTML = (NUM / 907  );
+            document.getElementById("03-mass").innerHTML = (NUM * 2.205);
+            document.getElementById("04-mass").innerHTML = (NUM / 1e+7 );
+            document.getElementsByClassName('ans-text-NUM').innerHTML = NUM;
+
+            document.getElementById("mass-1-formula").innerHTML = "* 1000 ";
+            document.getElementById("mass-2-formula").innerHTML = "/ 907  ";
+            document.getElementById("mass-3-formula").innerHTML = "* 2.205";
+            document.getElementById("mass-4-formula").innerHTML = "/ 1e+7 or 100,000,00";
+
+            Hide(UNIT);
+        } if (UNIT === 'G') {
+            document.getElementById("mass-1-text").innerHTML = "G to KG:";
+            document.getElementById("mass-2-text").innerHTML = "G to TON:";
+            document.getElementById("mass-3-text").innerHTML = "G to LBS:";
+            document.getElementById("mass-4-text").innerHTML = "G to MG:";
+
+            document.getElementById("mass-5-text").innerHTML = "G to KG:";
+            document.getElementById("mass-6-text").innerHTML = "G to TON:";
+            document.getElementById("mass-7-text").innerHTML = "G to LBS:";
+            document.getElementById("mass-8-text").innerHTML = "G to MG:";
+
+            document.getElementById("01-mass").innerHTML = (NUM / 1000  );
+            document.getElementById("02-mass").innerHTML = (NUM / 907185);
+            document.getElementById("03-mass").innerHTML = (NUM / 454   );
+            document.getElementById("04-mass").innerHTML = (NUM * 1000  );
+            document.getElementsByClassName('ans-text-NUM').innerHTML = NUM;
+
+            document.getElementById("mass-1-formula").innerHTML = "/ 1000  ";
+            document.getElementById("mass-2-formula").innerHTML = "/ 907185";
+            document.getElementById("mass-3-formula").innerHTML = "/ 454   ";
+            document.getElementById("mass-4-formula").innerHTML = "* 1000  ";
+
+            Hide(UNIT);
+        } if (UNIT === 'TON') {
+            document.getElementById("mass-1-text").innerHTML = "TON to G:";
+            document.getElementById("mass-2-text").innerHTML = "TON to KG:";
+            document.getElementById("mass-3-text").innerHTML = "TON to LBS:";
+            document.getElementById("mass-4-text").innerHTML = "TON to MG:";
+
+            document.getElementById("mass-5-text").innerHTML = "TON to G:";
+            document.getElementById("mass-6-text").innerHTML = "TON to KG:";
+            document.getElementById("mass-7-text").innerHTML = "TON to LBS:";
+            document.getElementById("mass-8-text").innerHTML = "TON to MG:";
+
+            document.getElementById("01-mass").innerHTML = (NUM * 907195  );
+            document.getElementById("02-mass").innerHTML = (NUM * 907     );
+            document.getElementById("03-mass").innerHTML = (NUM * 2000    );
+            document.getElementById("04-mass").innerHTML = (NUM * 9.072e+8);
+            document.getElementsByClassName('ans-text-NUM').innerHTML = NUM;
+
+            document.getElementById("mass-1-formula").innerHTML = "* 907195  ";
+            document.getElementById("mass-2-formula").innerHTML = "* 907     ";
+            document.getElementById("mass-3-formula").innerHTML = "* 2000    ";
+            document.getElementById("mass-4-formula").innerHTML = "* 9.072e+8";
+
+            Hide(UNIT);
+        } if (UNIT === 'LBS') {
+            document.getElementById("mass-1-text").innerHTML = "LBS to G:";
+            document.getElementById("mass-2-text").innerHTML = "LBS to KG:";
+            document.getElementById("mass-3-text").innerHTML = "LBS to TON:";
+            document.getElementById("mass-4-text").innerHTML = "LBS to MG:";
+
+            document.getElementById("mass-5-text").innerHTML = "LBS to G:";
+            document.getElementById("mass-6-text").innerHTML = "LBS to KG:";
+            document.getElementById("mass-7-text").innerHTML = "LBS to TON:";
+            document.getElementById("mass-8-text").innerHTML = "LBS to MG:";
+
+            document.getElementById("01-mass").innerHTML = (NUM * 454    );
+            document.getElementById("02-mass").innerHTML = (NUM / 2.205  );
+            document.getElementById("03-mass").innerHTML = (NUM / 2000   );
+            document.getElementById("04-mass").innerHTML = (NUM / 453_592);
+            document.getElementsByClassName('ans-text-NUM').innerHTML = NUM;
+
+            document.getElementById("mass-1-formula").innerHTML = "* 454    ";
+            document.getElementById("mass-2-formula").innerHTML = "/ 2.205  ";
+            document.getElementById("mass-3-formula").innerHTML = "/ 2000   ";
+            document.getElementById("mass-4-formula").innerHTML = "/ 453_592";
+
+            Hide(UNIT);
+        } if (UNIT === 'MG') {
+            document.getElementById("mass-1-text").innerHTML = "MG to G:";
+            document.getElementById("mass-2-text").innerHTML = "MG to KG:";
+            document.getElementById("mass-3-text").innerHTML = "MG to TON:";
+            document.getElementById("mass-4-text").innerHTML = "MG to LBS:";
+
+            document.getElementById("mass-5-text").innerHTML = "MG to G:";
+            document.getElementById("mass-6-text").innerHTML = "MG to KG:";
+            document.getElementById("mass-7-text").innerHTML = "MG to TON:";
+            document.getElementById("mass-8-text").innerHTML = "MG to LBS:";
+
+            document.getElementById("01-mass").innerHTML = (NUM / 1000         );
+            document.getElementById("02-mass").innerHTML = (NUM / 100_000_0    );
+            document.getElementById("03-mass").innerHTML = (NUM / 100_000_000_0);
+            document.getElementById("04-mass").innerHTML = (NUM / 1453592      );
+            document.getElementsByClassName('ans-text-NUM').innerHTML = NUM;
+
+            document.getElementById("mass-1-formula").innerHTML = "/ 1000         ";
+            document.getElementById("mass-2-formula").innerHTML = "/ 100_000_0    ";
+            document.getElementById("mass-3-formula").innerHTML = "/ 100_000_000_0";
+            document.getElementById("mass-4-formula").innerHTML = "/ 1453592      ";
+
+            Hide(UNIT);
+        };
+    };
+};
