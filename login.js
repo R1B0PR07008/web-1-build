44225950ec9aa597d3f28521fc3d117ade09469a
R1B0PR07008 <104583138+R1B0PR07008@users.noreply.github.com>
2022-11-06 17:50:04 -0600

    not done (just in case something goes wrong)

--- login.js
+++ login.js
@@ -1,67 +1,37 @@
-function DB(username, password) {
-    const sqlite3 = require('sqlite3').Database();
-    
-    let sql = 'INSERT INTO userData(Username, Password) VALUES(?,?)';
-    
-    // !open database in memory
-    let db = new sqlite3.Database('./User-DATA-DB.sqlite', sqlite3.OPEN_READWRITE, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log('Connected to the database.');
-    });
-    
-    db.run("CREATE TABLE IF NOT EXISTS userData (Username, Password)");
-    
-    //   //!Variables for INPUT
-    let UserName = username;
-    let Password = password;  
-    
-    console.log('btn-register-1 Pressed');
-    sql = 'INSERT INTO userData(Username, Password) VALUES(?,?)';
-      
-    db.run(
-    sql, 
-    [UserName, Password], 
-    (err) => {
-    if (err) return console.error(err.message)
-        
-    console.log('DATA ADDED')
-    }); 
-    
-    sql = 'SELECT * FROM userData';
-    
-    
-    
-    db.all(sql, [], (err, rows)=>{
-      if (err) return console.error(err.message)
-    
-      rows.forEach(row => {
-        console.log(row)
-      });
-    })
-    
-    // !close the database connection
-    db.close((err) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      console.log('Close the database connection.');
-    });
-    };
-    
+const fs = require("fs");
+fs.readFile("data.csv", "utf-8", (err, data) => {
+  if (err) console.log(err);
+  else ;
+});
 
-if (document.URL.includes("login.html")) {
+// this doesn't work !!!!!!🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬
 
-    function username() {
-        let username = document.getElementById("Username-Login").value;
-        return username
-    };
-    function password() {
-        let password = document.getElementById("Password-Login").value;
-        return password
-    };
+let Username = 'test';
+let Password = 'test2p';
 
-    document.getElementById("BTN-1-LGIN").onclick = DB(username(), password());
+user = 'Username,Password \n' + Username + ',' + Password;
+console.log('USER \n' + user + '\n')
+
+console.log('DATA \n' + data + '\n')
+
+// if ( === user) {
+//   console.log('You are in!' + '\n')
+// } else {
+//   console.log('You are not in!' + '\n')
+// }
+
+
+// if (document.URL.includes("login.html")) {
+
+//     function username() {
+//         let username = document.getElementById("Username-Login").value;
+//         return username
+//     };
+//     function password() {
+//         let password = document.getElementById("Password-Login").value;
+//         return password
+//     };
+
+//     document.getElementById("BTN-1-LGIN").onclick = DB(username(), password());
+// };
   
-};
