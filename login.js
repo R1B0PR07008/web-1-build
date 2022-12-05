3a0cccef448a584c2bfa07187127b1b45cb2756a
R1B0PR07008 <104583138+R1B0PR07008@users.noreply.github.com>
2022-12-05 14:25:51 -0800

    mostly release ready

--- login.js
+++ login.js
@@ -1,37 +1,14 @@
-const fs = require("fs");
-fs.readFile("data.csv", "utf-8", (err, data) => {
-  if (err) console.log(err);
-  else ;
+// * Making a login system for my website. It is a headache 
+
+let mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
 });
 
-// this doesn't work !!!!!!🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬
-
-let Username = 'test';
-let Password = 'test2p';
-
-user = 'Username,Password \n' + Username + ',' + Password;
-console.log('USER \n' + user + '\n')
-
-console.log('DATA \n' + data + '\n')
-
-// if ( === user) {
-//   console.log('You are in!' + '\n')
-// } else {
-//   console.log('You are not in!' + '\n')
-// }
-
-
-// if (document.URL.includes("login.html")) {
-
-//     function username() {
-//         let username = document.getElementById("Username-Login").value;
-//         return username
-//     };
-//     function password() {
-//         let password = document.getElementById("Password-Login").value;
-//         return password
-//     };
-
-//     document.getElementById("BTN-1-LGIN").onclick = DB(username(), password());
-// };
-  
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
